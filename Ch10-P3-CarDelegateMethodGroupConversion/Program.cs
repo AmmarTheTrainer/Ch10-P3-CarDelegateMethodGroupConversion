d9da7a8c1350b946d8c510a955b3bf057d488eea
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-09-30 13:03:11 +0500

    final commit

--- Ch10-P3-CarDelegateMethodGroupConversion/Program.cs
+++ Ch10-P3-CarDelegateMethodGroupConversion/Program.cs
@@ -10,6 +10,37 @@ namespace Ch10_P3_CarDelegateMethodGroupConversion
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Method Group Conversion *****\n");
+            Car c1 = new Car();
+
+            // Register the simple method name.
+            c1.RegisterWithCarEngine(CallMeHere);
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+            // Unregister the simple method name.
+            c1.UnRegisterWithCarEngine(CallMeHere);
+            // No more notifications!
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        static void CallMeHere(string msg)
+        {
+            Console.WriteLine("=> Message from Car: {0}", msg);
+        }
+        // This is the target for incoming events.
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("***********************************\n");
+        }
+        public static void OnCarEngineEvent2(string msg)
+        {
+            Console.WriteLine("=> {0}", msg.ToUpper());
         }
     }
 }
